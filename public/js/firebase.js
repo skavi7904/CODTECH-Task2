1d0216154699852230888e05a1ccd6ff845196d6
Kaviyarasan S <113685613+skavi7904@users.noreply.github.com>
2024-08-24 18:07:00 +0530

    Update firebase.js

--- public/js/firebase.js
+++ public/js/firebase.js
@@ -8,12 +8,6 @@ import {
   onAuthStateChanged,
 } from "https://www.gstatic.com/firebasejs/10.13.0/firebase-auth.js";
 let firebaseConfig = {
-  apiKey: "AIzaSyCclfXYabSu979d__RT9SdEOnt25W8niZg",
-  authDomain: "blogwebsite-8efb5.firebaseapp.com",
-  projectId: "blogwebsite-8efb5",
-  storageBucket: "blogwebsite-8efb5.appspot.com",
-  messagingSenderId: "795826317703",
-  appId: "1:795826317703:web:73a3ad1259f2a4a8820b22",
 };
 
 // Initialize Firebase
